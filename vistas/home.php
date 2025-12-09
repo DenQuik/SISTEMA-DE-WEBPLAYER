093776828839137a457028f4abd2e045dd83fefc
denquik <denquik@hotmail.com>
2025-12-09 03:06:24 -0400

    Se agrega el mensaje en el home

--- vistas/home.php
+++ vistas/home.php
@@ -1,6 +1,6 @@
 <div class="container is-fluid">
 	<h1>­</h1>
-	<h1 class="title">Home</h1>
+	<h1 class="title">Home este es el primer cambio para ver si se sube al github</h1>
 	<h2 class="subtitle">¡Bienvenido <?php echo $_SESSION['nombre']." ".$_SESSION['apellido']; ?>!</h2>
 </div>
 
